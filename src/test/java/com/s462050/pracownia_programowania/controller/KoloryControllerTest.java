7b059271d94dbc43a75c351d3da1cfe9613afcfd
Piotr Jankowski <piotr.jankowski1@interia.pl>
2022-01-30 22:53:54 +0100

    Drugi test dla modelu Kolory: addKolor

--- src/test/java/com/s462050/pracownia_programowania/controller/KoloryControllerTest.java
+++ src/test/java/com/s462050/pracownia_programowania/controller/KoloryControllerTest.java
@@ -1,13 +1,16 @@
 package com.s462050.pracownia_programowania.controller;
 
+import com.s462050.pracownia_programowania.model.Kolory;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 
 import javax.annotation.PostConstruct;
 
+import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -36,6 +39,19 @@ class KoloryControllerTest {
 
     @Test
     void addKolor() {
+        Kolory kolory = new Kolory();
+        kolory.setNazwaKoloru("Testowy");
+        Kolory nowyKolor = given()
+                .when()
+                .contentType(String.valueOf(MediaType.APPLICATION_JSON))
+                .body(kolory)
+                .post(uri + "/kolory/")
+                .then()
+                .statusCode(201)
+                .extract().body().as(Kolory.class);
+        assertNotNull(nowyKolor);
+        assertNotNull(nowyKolor.getID());
+
     }
 
     @Test
